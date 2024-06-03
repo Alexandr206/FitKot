ee8749cf6c18dec6a56ec04b0c95e3b6b4b90a92
Александр <sasho1811@mail.ru>
2024-06-03 21:35:32 +0300

    A1.4
    Косметика опять

--- app/src/main/java/com/example/fitkot/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/fitkot/activity/ProfileActivity.kt
@@ -57,6 +57,31 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(this, UsersListActivity::class.java)
             startActivity(intent)
         }
+
+        val navigation_news: Button = findViewById(R.id.navigation_news)
+        val navigation_map: Button = findViewById(R.id.navigation_map)
+        val navigation_messages: Button = findViewById(R.id.navigation_messages)
+        val navigation_trainers: Button = findViewById(R.id.navigation_trainers)
+        val navigation_profile: Button = findViewById(R.id.navigation_profile)
+        navigation_news.setOnClickListener {
+            startActivity(Intent(this, NewsActivity::class.java))
+        }
+
+        navigation_map.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        navigation_messages.setOnClickListener {
+            startActivity(Intent(this, MessagesActivity::class.java))
+        }
+
+        navigation_trainers.setOnClickListener {
+            startActivity(Intent(this, TrainersActivity::class.java))
+        }
+
+        navigation_profile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/example/fitkot/activity/TrainersActivity.kt
@@ -0,0 +1,44 @@
+package com.example.fitkot.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitkot.R
+
+class TrainersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trainers)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Тренера"
+
+        val navigation_news: Button = findViewById(R.id.navigation_news)
+        val navigation_map: Button = findViewById(R.id.navigation_map)
+        val navigation_messages: Button = findViewById(R.id.navigation_messages)
+        val navigation_trainers: Button = findViewById(R.id.navigation_trainers)
+        val navigation_profile: Button = findViewById(R.id.navigation_profile)
+        navigation_news.setOnClickListener {
+            startActivity(Intent(this, NewsActivity::class.java))
+        }
+
+        navigation_map.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        navigation_messages.setOnClickListener {
+            startActivity(Intent(this, MessagesActivity::class.java))
+        }
+
+        navigation_trainers.setOnClickListener {
+            startActivity(Intent(this, TrainersActivity::class.java))
+        }
+
+        navigation_profile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+    }
+
+}
